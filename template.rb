9492c109d480187982d6501db401a532b39a73fd
Caio Ergos <caioergos@gmail.com>
2017-10-20 22:28:14 -0300

    setup database sample yml

--- template.rb
+++ template.rb
@@ -12,11 +12,7 @@ def apply_template!
   copy_file 'gitignore', '.gitignore', force: true
 
   apply 'app/template.rb'
-end
-
-def preexisting_git_repo?
-  @preexisting_git_repo ||= File.exist?(".git")
-  @preexisting_git_repo == true
+  apply 'config/template.rb'
 end
 
 def assert_minimum_rails_version
@@ -27,6 +23,27 @@ def assert_minimum_rails_version
   prompt = "This template requires Rails #{RAILS_REQUIREMENT}. "\
     "You are using #{rails_version}. Continue anyway?"
   exit 1 if no?(prompt)
+end
+
+def postgresql_username
+  if yes?('Do you want to use a custom database username for development and test?')
+    say('Type your custom username below...')
+    username = ask('username: ')
+    "username: #{username}"
+  end
+end
+
+def postgresql_password
+  if yes?('Do you wan to use a custom database password for development and test?')
+    say('Type your custom password below...')
+    password = ask('password: ')
+    "password: #{password}"
+  end
+end
+
+def preexisting_git_repo?
+  @preexisting_git_repo ||= File.exist?(".git")
+  @preexisting_git_repo == true
 end
 
 apply_template!
