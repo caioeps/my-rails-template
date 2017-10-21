3f4bbfa5915569eff59a0e675d5dcd3697e55af6
Caio Ergos <caioergos@gmail.com>
2017-10-20 22:49:40 -0300

    move postgresql username and password to main template

---
+++ config/initializers/locale.rb
@@ -0,0 +1,4 @@
+Rails.application.config.i18n.default_locale = :'pt-BR'
+Rails.application.config.i18n.load_path +=
+  Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+
--- config/template.rb
+++ config/template.rb
@@ -1,18 +1,6 @@
 remove_file 'config/database.yml'
 template 'config/database.sample.yml.tt'
 
-def postgresql_username
-  if yes?('Do you want to use a custom database username for development and test?')
-    say('Type your custom username below...')
-    username = ask('username: ')
-    "username: #{username}"
-  end
-end
+copy_file 'config/initializers/locale.rb'
+copy_file 'config/locales/pt-BR.yml'
 
-def postgresql_password
-  if yes?('Do you wan to use a custom database password for development and test?')
-    say('Type your custom password below...')
-    password = ask('password: ')
-    "password: #{password}"
-  end
-end
